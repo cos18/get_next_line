0c1b5b74f4c34c17d61d6ed4e6b47564b38d2761
Sungwoo Park <codest99@gmail.com>
2020-03-23 23:17:53 +0900

    All clear but some tiny error
    Make all in one day!
    But can't process when there is no \n at last

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,62 @@
 /*   By: sunpark <sunpark@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/22 16:19:58 by sunpark           #+#    #+#             */
-/*   Updated: 2020/03/22 16:21:43 by sunpark          ###   ########.fr       */
+/*   Updated: 2020/03/23 23:07:35 by sunpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(int fd, char **line)
+static char	*set_new_str(char **line, char *save)
 {
+	int		size;
+	int		locate;
 
+	size = 0;
+	while (save[size] && save[size] != '\n')
+		size++;
+	*line = (char *)malloc(size + 1);
+	locate = 0;
+	while (locate < size)
+	{
+		(*line)[locate] = save[locate];
+		locate++;
+	}
+	(*line)[locate] = '\0';
+	return ((save[size] == '\n') ? ft_strdup(save + size + 1) : ft_strnul());
+}
+
+int			final_reset(char **line)
+{
+	*line = ft_strnul();
+	return (0);
+}
+
+int			get_next_line(int fd, char **line)
+{
+	int			len;
+	char		buff[BUFFER_SIZE + 1];
+	static char	*str[FD_SIZE];
+	char		*tmp_str;
+
+	if (fd < 0 || !line || fd > FD_SIZE || BUFFER_SIZE <= 0)
+		return (-1);
+	if (!str[fd])
+		str[fd] = ft_strnul();
+	while (!(ft_strchr(str[fd], '\n')) && (len = read(fd, buff, BUFFER_SIZE)) > 0)
+	{
+		buff[len] = '\0';
+		tmp_str = ft_strjoin(str[fd], buff);
+		free(str[fd]);
+		str[fd] = tmp_str;
+	}
+	if (len < 0)
+		return (-1);
+	if (len == 0 && ft_strlen(str[fd]) == 0)
+		return (final_reset(line));
+	tmp_str = set_new_str(line, str[fd]);
+	free(str[fd]);
+	if (*tmp_str)
+		str[fd] = tmp_str;
+	return ((*tmp_str) ? 1 : 0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,27 @@
 /*   By: sunpark <sunpark@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/22 16:20:16 by sunpark           #+#    #+#             */
-/*   Updated: 2020/03/22 16:21:08 by sunpark          ###   ########.fr       */
+/*   Updated: 2020/03/23 22:24:32 by sunpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+# define FD_SIZE 10000
+
 int		get_next_line(int fd, char **line);
+char	*ft_strnul(void);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
 
 #endif
